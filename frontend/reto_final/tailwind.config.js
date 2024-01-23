2cce00f72d1258b5b051456291100ef1491431e0
DiegoP2001 <di.piedra@aulanz.net>
2024-01-23 12:20:34 +0100

    Implementando Modales de Navbar y añadiendo funcionalidad de video al hacer hover en Gallery

--- frontend/reto_final/tailwind.config.js
+++ frontend/reto_final/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, js, ts, jsx, tsx}",
+    "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -14,6 +19,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
 
